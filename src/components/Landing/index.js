dced9801cb5af9c7c3088fd39c541aa1970335b7
KKallidromitis <kkarraskallidromitis@gmail.com>
2020-01-24 01:29:08 -0500

    launch page updated

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -3,7 +3,7 @@ import logo from '../images/logo.png';
 import { Icon } from 'react-icons-kit'
 import { home } from 'react-icons-kit/icomoon/home'
 import './index.css';
-import {Container,Row,Col} from 'react-bootstrap';
+import {Container,Row,Col,Button} from 'react-bootstrap';
 
 function Landing() {
   return (
@@ -12,35 +12,46 @@ function Landing() {
       <img src={logo} className="logo" alt="logo_collide"/>
         <Container className='c1'>
           <Row>
-            <Col><h1 className="c1t1">Do fun stuff with cool people</h1></Col>
+            <Col><p className="c1t1">Grab drinks and meet cool people</p></Col>
           </Row>
           <Row>
-          <Col><h3 className="c1t2">Whether you are visiting or new in town or looking to expand your social circle. Collide is the least awkward and best way to meet new people.</h3></Col>
+            <Col><h3 className="c1t2">New to the city, or looking to expand your social circle? Meet cool people through casual happy hours we organize around the city.</h3></Col>
           </Row>
-          <Col md={6}/>
-          </Container>
+          <Row className='con_btn'>
+            <Button variant="primary" size="lg" className='btn_fb'>Connect with Facebook</Button>
+          </Row>
+        </Container>
       </div>
       <div className='w2'>
         <Container className='c2'>
           <Row >
-          <Col md={4}>
-          <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100vh'}} ><i class="circle-icon"><Icon icon={home}/></i></div>
-          <h1 className="c2t1 ">Convenient</h1>
-          <h1 className="c1t1">We host small group happy hours within walking distance of your home and which fit around your schedule</h1>
+          <Col md={{ span: 4}}>
+            <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}} className='icons'><i class="circle-icon"><Icon size={26} icon={home}/></i></div>
+            <h1 className="c2t1">Convenient</h1>
+            <p className="c2t2">We host small group happy hours within walking distance of your home and which fit around your schedule</p>
           </Col>
-
-          <Col md={4}><h1 className="c1t1">Do fun stuff with cool people</h1></Col>
-          <Col md={4}><h1 className="c1t1">Do fun stuff with cool people</h1></Col>
+          <Col md={{ span: 4}}>
+            <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}} className='icons'><i class="circle-icon"><Icon size={26} icon={home}/></i></div>
+            <h1 className="c2t1">Connect</h1>
+            <p className="c2t2">Meet people who live nearby, and have similar interests and availability. See them again at another event!</p>
+          </Col>
+          <Col md={{ span: 4}}>
+            <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}} className='icons'><i class="circle-icon"><Icon size={26} icon={home}/></i></div>
+            <h1 className="c2t1">Casual</h1>
+            <p className="c2t2">Come whenever, and leave whenever. There is no agenda, no set schedule, and no pressure or awkward conversation</p>
+          </Col>
           </Row>
         </Container>
       </div>
       <div className='w3'>
         <Container className='c3'>
-          <Row>
-          <Col><h1 className="c1t1">Do fun stuff with cool people</h1></Col>
-          <Col><h1 className="c1t1">Do fun stuff with cool people</h1></Col>
-          <Col><h1 className="c1t1">Do fun stuff with cool people</h1></Col>
-          </Row>
+          <Col>
+            <Row className="how"><h1 className="c3t1">How does it work?</h1></Row>
+            <Row>
+              <h1 className="c3t2">Step1</h1>
+              <p className="c3t3">Provide your zip code and availability and we'll send you updates about the happy hours we host that are convenient for your location and schedule</p>
+            </Row>
+          </Col>
         </Container>
       </div>
       <div className='w4'>
