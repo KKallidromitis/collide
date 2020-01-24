e955472f8b1eeb290c9ae8a764524cc220391c8d
KKallidromitis <kkarraskallidromitis@gmail.com>
2020-01-23 23:11:51 -0500

    firebase host

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import logo from '../images/logo.png';
+import { Icon } from 'react-icons-kit'
+import { home } from 'react-icons-kit/icomoon/home'
 import './index.css';
 import {Container,Row,Col} from 'react-bootstrap';
 
@@ -17,6 +19,43 @@ function Landing() {
           </Row>
           <Col md={6}/>
           </Container>
+      </div>
+      <div className='w2'>
+        <Container className='c2'>
+          <Row >
+          <Col md={4}>
+          <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100vh'}} ><i class="circle-icon"><Icon icon={home}/></i></div>
+          <h1 className="c2t1 ">Convenient</h1>
+          <h1 className="c1t1">We host small group happy hours within walking distance of your home and which fit around your schedule</h1>
+          </Col>
+
+          <Col md={4}><h1 className="c1t1">Do fun stuff with cool people</h1></Col>
+          <Col md={4}><h1 className="c1t1">Do fun stuff with cool people</h1></Col>
+          </Row>
+        </Container>
+      </div>
+      <div className='w3'>
+        <Container className='c3'>
+          <Row>
+          <Col><h1 className="c1t1">Do fun stuff with cool people</h1></Col>
+          <Col><h1 className="c1t1">Do fun stuff with cool people</h1></Col>
+          <Col><h1 className="c1t1">Do fun stuff with cool people</h1></Col>
+          </Row>
+        </Container>
+      </div>
+      <div className='w4'>
+        <Container className='c4'>
+          <Row>
+          <Col><h1 className="c1t1">Do fun stuff with cool people</h1></Col>
+          <Col><h1 className="c1t1">Do fun stuff with cool people</h1></Col>
+          <Col><h1 className="c1t1">Do fun stuff with cool people</h1></Col>
+          </Row>
+        </Container>
+      </div>
+      <div>
+        <Container className='footer'>
+          <p>© 2019 All rights reserved </p>
+        </Container>
       </div>
     </div>
   );
