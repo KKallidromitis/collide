c302c5a2136c2d44deb3875052818c5d8ec0a6aa
KKallidromitis <kkarraskallidromitis@gmail.com>
2020-01-29 23:45:51 -0500

    launch_f1

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import logo from '../images/logo.png';
 import { Icon } from 'react-icons-kit'
-import { home } from 'react-icons-kit/icomoon/home'
+import {iosPeople} from 'react-icons-kit/ionicons/iosPeople'
+import {androidCompass} from 'react-icons-kit/ionicons/androidCompass'
+import {androidBar} from 'react-icons-kit/ionicons/androidBar'
 import './index.css';
-import {Container,Row,Col,Button} from 'react-bootstrap';
+import {Container,Row,Col,Button,Carousel} from 'react-bootstrap';
 
 function Landing() {
   return (
@@ -26,17 +28,17 @@ function Landing() {
         <Container className='c2'>
           <Row >
           <Col md={{ span: 4}}>
-            <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}} className='icons'><i class="circle-icon"><Icon size={26} icon={home}/></i></div>
+            <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}} className='icons'><i class="circle-icon"><Icon size={26} icon={iosPeople}/></i></div>
             <h1 className="c2t1">Convenient</h1>
             <p className="c2t2">We host small group happy hours within walking distance of your home and which fit around your schedule</p>
           </Col>
           <Col md={{ span: 4}}>
-            <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}} className='icons'><i class="circle-icon"><Icon size={26} icon={home}/></i></div>
+            <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}} className='icons'><i class="circle-icon"><Icon size={26} icon={androidCompass}/></i></div>
             <h1 className="c2t1">Connect</h1>
             <p className="c2t2">Meet people who live nearby, and have similar interests and availability. See them again at another event!</p>
           </Col>
           <Col md={{ span: 4}}>
-            <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}} className='icons'><i class="circle-icon"><Icon size={26} icon={home}/></i></div>
+            <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}} className='icons'><i class="circle-icon"><Icon size={26} icon={androidBar}/></i></div>
             <h1 className="c2t1">Casual</h1>
             <p className="c2t2">Come whenever, and leave whenever. There is no agenda, no set schedule, and no pressure or awkward conversation</p>
           </Col>
@@ -51,14 +53,17 @@ function Landing() {
               <h1 className="c3t2">Step 1:</h1>
               <p className="c3t3">Provide your zip code and availability and we'll send you updates about the happy hours we host that are convenient for your location and schedule.</p>
             </Row>
-             <Row>
+            <hr width="80%"/>
+            <Row>
               <h1 className="c3t2">Step 2:</h1>
               <p className="c3t3">Each event will have a dedicated liaison to help you locate the group, make introductions, and answer any questions.</p>
             </Row>
+            <hr width="80%"/>
             <Row>
               <h1 className="c3t2">Step 3:</h1>
               <p className="c3t3">Come whenever and leave whenever. Our events are unstructured and casual, and meant to ensure everyone feels comfortable and included.</p>
             </Row>
+            <hr width="80%"/>
             <Row>
               <h1 className="c3t2">Step 4:</h1>
               <p className="c3t3">We regularly host events so you’ll likely see the same people multiple times allowing you to slowly build connections and develop new friendships.</p>
@@ -68,9 +73,24 @@ function Landing() {
       </div>
       <div className='w4'>
         <Container className='c4'>
+          <Row className='carousel'>
+          <Carousel>
+            <Carousel.Item>
+              <img className="d-block w-100" src={require("../images/car/d2.jpg")} alt="First slide"/>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img className="d-block w-100" src={require("../images/car/d1.jpg")} alt="First slide"/>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img className="d-block w-100" src={require("../images/car/d3.jpeg")} alt="First slide"/>
+            </Carousel.Item>
+          </Carousel>
+          </Row>
           <Row>
-          <h1 className="c1t1">Sounds cool? Try it out!</h1>
-          <Button variant="primary" size="lg" className='btn_fb'>Connect with Facebook</Button>
+            <h1 className="c4t1">Sounds cool? Try it out!</h1>
+          </Row>
+          <Row className='con_btn2'>
+            <Button variant="primary" size="lg" className='btn_fb'>Connect with Facebook</Button>
           </Row>
         </Container>
       </div>
