bc6a744b66aa536509ab054581be93e30b9156ff
Kushal Singh <kushal16.singh@gmail.com>
2020-01-30 21:49:06 -0500

    Adding route to Home Page

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,34 +1,25 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
+//import Navigation from '../Navigation/index';
 import LandingPage from '../Landing/index';
-import * as ROUTES from '../../constants/routes';
-
-/*import SignUpPage from '../SignUp/index';
+import SignUpPage from '../SignUp/index';
 import SignInPage from '../SignIn/index';
 import PasswordForgetPage from '../PasswordForget/index';
 import HomePage from '../Home/index';
 import AccountPage from '../Account/index';
-import AdminPage from '../Admin/index'; */
-
-/* const App = () => (
-  <Router>
-    <div>
-      <Route exact path={ROUTES.LANDING} component={LandingPage} />
-      <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
-      <Route path={ROUTES.SIGN_IN} component={SignInPage} />
-      <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
-      <Route path={ROUTES.HOME} component={HomePage} />
-      <Route path={ROUTES.ACCOUNT} component={AccountPage} />
-      <Route path={ROUTES.ADMIN} component={AdminPage} />
-    </div>
-  </Router> */
-
+import AdminPage from '../Admin/index';
+import * as ROUTES from '../../constants/routes';
 const App = () => (
   <Router>
     <div>
       <Route exact path={ROUTES.LANDING} component={LandingPage} />
+      {/* <Route path={ROUTES.SIGN_UP} component={SignUpPage} /> */}
+      <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+      {/* <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} /> */}
+      <Route path={ROUTES.HOME} component={HomePage} />
+      {/* <Route path={ROUTES.ACCOUNT} component={AccountPage} /> */}
+      {/* <Route path={ROUTES.ADMIN} component={AdminPage} /> */}
     </div>
   </Router>
-
 );
 export default App;
